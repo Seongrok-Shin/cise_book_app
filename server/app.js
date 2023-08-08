9c2cb1c9ce670d5518e2487b03cd3a4acc579248
SeongrokShin <ssr7324@gmail.com>
2023-08-09 02:27:46 +1200

    (Add) default.json for db and change db variables

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 // app.js
 
 const express = require('express');
-const connectDB = require('./config/db');
+const connectDB = require("./config/db");
 const cors = require('cors');
 
 // routes
@@ -13,10 +13,7 @@ const app = express();
 connectDB();
 
 // cors
-app.use(cors({
-    origin: ["https://cise-book-app-api.vercel.app"],
-    credentials: true
-}));
+app.use(cors({ origin: true, credentials: true }));
 
 // Init Middleware
 app.use(express.json({ extended: false }));
@@ -28,4 +25,4 @@ app.use('/api/books', books);
 
 const port = process.env.PORT || 8082;
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => console.log(`Server running n port ${port}`));
