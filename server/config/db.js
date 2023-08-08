dc7c0e5fe7f185a4caa11edb7a9a74030d8138f7
SeongrokShin <ssr7324@gmail.com>
2023-08-09 02:32:45 +1200

    Changed!!!!

--- server/config/db.js
+++ server/config/db.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 // const db = process.env.MONGO_URI;
 const config = require('config');
 const db = "mongodb+srv://SeongrokShin:YtTy42w5ksqIF6Gl@cluster0.ohhlzqd.mongodb.net/?retryWrites=true&w=majority"
+
 const connectDB = async () => {
     try {
         mongoose.set('strictQuery', true);
