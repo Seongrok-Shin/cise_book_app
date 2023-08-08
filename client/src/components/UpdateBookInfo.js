9edbfb3f4acaabac99ee627672a6433fdd0242bd
SeongrokShin <ssr7324@gmail.com>
2023-08-09 02:44:46 +1200

    (BugFix) axios link was wrong

--- client/src/components/UpdateBookInfo.js
+++ client/src/components/UpdateBookInfo.js
@@ -18,7 +18,7 @@ function UpdateBookInfo(props) {
 
     useEffect(() => {
         axios
-            .get(`cise-book-app-api.vercel.app/api/books/${id}`)
+            .get(`https://cise-book-app-api.vercel.app/api/books/${id}`)
             .then((res) => {
                 setBook({
                     title: res.data.title,
@@ -51,7 +51,7 @@ function UpdateBookInfo(props) {
         };
 
         axios
-            .put(`cise-book-app-api.vercel.app/api/books/${id}`, data)
+            .put(`https://cise-book-app-api.vercel.app/api/books/${id}`, data)
             .then((res) => {
                 navigate(`/show-book/${id}`);
             })
